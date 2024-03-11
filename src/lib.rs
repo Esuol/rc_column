4d9b9cc5b824abec22fdc5e971aa48ed6b20892c
berlinen <1292393108@11.com>
2024-03-11 16:07:36 +0800

    feat: impl from for cell

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,22 @@ use unicode_width::UnicodeWidthStr;
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
 pub enum Alignment {
     Left,
-
     Right,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Cell {
+    pub contents: String,
+    pub width: usize,
+    pub alignment: Alignment,
+}
+
+impl From<String> for Cell {
+    fn from(string: String) -> Self {
+        Self {
+            width: UnicodeWidthStr::width(&*string),
+            contents: string,
+            alignment: Alignment::Left,
+        }
+    }
 }
